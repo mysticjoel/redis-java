26bf80efea08b2e5e79c2d38b1b349cbc84362fe
mysticjoe <joeljoseph1717@gmail.com>
2025-07-09 15:24:14 +0530

    [getset]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Main {
 
@@ -31,12 +32,13 @@ class ClientHandler implements Runnable {
     public void run() {
         try (
                 BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                OutputStream outputStream = clientSocket.getOutputStream()
+                OutputStream writer = clientSocket.getOutputStream()
         ) {
             String line;
+            HashMap<String,String> map = new HashMap<>();
             while ((line = reader.readLine()) != null) {
                 if (line.trim().equalsIgnoreCase("PING")) {
-                    outputStream.write("+PONG\r\n".getBytes());
+                    writer.write("+PONG\r\n".getBytes());
                     continue;
                 }
 
@@ -45,7 +47,24 @@ class ClientHandler implements Runnable {
                     reader.readLine(); // skip $length
                     String value = reader.readLine(); // get the actual value
                     String response = "$" + value.length() + "\r\n" + value + "\r\n";
-                    outputStream.write(response.getBytes());
+                    writer.write(response.getBytes());
+                }
+                if(line.trim().equalsIgnoreCase("SET")){
+                    System.out.println(line);
+                    reader.readLine();
+                    map.put(reader.readLine(),reader.readLine());
+                    System.out.println(map);
+                    writer.write(("+OK\r\n".getBytes()));
+                    continue;
+                }
+                if(line.trim().equalsIgnoreCase("GET")){
+                    System.out.println(line);
+                    reader.readLine();
+                    String response = reader.readLine();
+                    String value = map.get(response);
+                    String real = "$" + response.length() + "\r\n" + value + "\r\n";
+                    writer.write(real.getBytes());
+                    //continue;
                 }
             }
                 // Optionally, handle other Redis-like commands here
